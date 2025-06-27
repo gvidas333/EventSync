67d6fba67b8205b7e0e4b114eaa7e177c86a3482
gvidas333 <gvidas.bac@gmail.com>
2025-06-27 12:56:21 +0300

    feat: added event controller

---
+++ src/main/java/com/github/gvidas333/eventsync/controller/EventController.java
@@ -0,0 +1,28 @@
+package com.github.gvidas333.eventsync.controller;
+
+import com.github.gvidas333.eventsync.model.Event;
+import com.github.gvidas333.eventsync.service.EventService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/events")
+@RequiredArgsConstructor
+public class EventController {
+
+    private final EventService eventService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Event createEvent(@RequestBody Event event) {
+        return eventService.createEvent(event);
+    }
+
+    @GetMapping
+    public List<Event> getAllEvents() {
+        return eventService.getAllEvents();
+    }
+}
