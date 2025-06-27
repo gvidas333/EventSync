3a4a1d35eb96c5097a45ef9d74df3e669452d8ed
gvidas333 <gvidas.bac@gmail.com>
2025-06-27 12:00:29 +0300

    feat: added repositories

--- src/main/java/com/github/gvidas333/eventsync/repository/FeedbackRepository.java
+++ src/main/java/com/github/gvidas333/eventsync/repository/FeedbackRepository.java
@@ -1,4 +1,14 @@
 package com.github.gvidas333.eventsync.repository;
 
-public interface FeedbackRepository {
+import com.github.gvidas333.eventsync.model.Feedback;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface FeedbackRepository extends JpaRepository<Feedback, UUID> {
+
+    List<Feedback> findByEventId(UUID eventId);
 }
