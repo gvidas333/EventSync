bc355909630f4a33c5e92b7ea8c75e1fb3191995
gvidas333 <gvidas.bac@gmail.com>
2025-06-28 15:47:22 +0300

    test: added EventServiceTests. Added NotNull checks for request dtos

--- src/main/java/com/github/gvidas333/eventsync/dto/CreateEventRequest.java
+++ src/main/java/com/github/gvidas333/eventsync/dto/CreateEventRequest.java
@@ -1,10 +1,13 @@
 package com.github.gvidas333.eventsync.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
 public class CreateEventRequest {
 
+    @NotBlank
     private String title;
+
     private String description;
 }
